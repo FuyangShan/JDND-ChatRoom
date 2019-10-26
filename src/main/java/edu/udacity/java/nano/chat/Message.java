1780b2dde5498074a4146d749bcc71857215dda1
Shan Fuyang <fuyang@Shans-MBP.attlocal.net>
2019-10-25 19:42:44 -0700

    Add Message Model

--- src/main/java/edu/udacity/java/nano/chat/Message.java
+++ src/main/java/edu/udacity/java/nano/chat/Message.java
@@ -4,5 +4,24 @@ package edu.udacity.java.nano.chat;
  * WebSocket message model
  */
 public class Message {
-    // TODO: add message model.
+     // TODO: add message model.
+    private String text;
+
+    //in case it is null?
+    public Message(){
+    }
+
+    //constructor
+    public Message(String text) {
+        this.text = text;
+    }
+
+    //getter and setter
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
